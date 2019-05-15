ee3a0b814bbefe2fbdfb4987a540a158203b8718
CamiloPacheco <cacomas@uninorte.edu.co>
2019-05-14 21:12:28 -0500

    six commit

--- postlogin/estudiante.php
+++ postlogin/estudiante.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 require '../db/conexion.php';
- 
+
 require 'base.php';
 
 $db=database::conectar();
-$categorias=$db->query("select Nombre,id from Categoria")->fetch_all(MYSQLI_ASSOC);
- 
+$categoria=$db->query("select Nombre,id from Categoria")->fetch_all(MYSQLI_ASSOC);
+
 $cursos=$db->query("SELECT * from curso ")->fetch_all(MYSQLI_ASSOC);
 session_start();
- 
+
 
 ?>
 
@@ -25,13 +25,13 @@ session_start();
         <meta name="description" content="">
 		<!-- Mobile Specific Meta -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
            <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet'>
-        
+
         <!-- Syntax Highlighter -->
         <link rel="stylesheet" type="text/css" href="syntax-highlighter/styles/shCore.css" media="all">
         <link rel="stylesheet" type="text/css" href="syntax-highlighter/styles/shThemeDefault.css" media="all">
-		
+
 		<!-- Font Awesome CSS-->
         <link rel="stylesheet" href="css/font-awesome.min.css">
         <!-- Normalize/Reset CSS-->
@@ -54,11 +54,11 @@ session_start();
 	<link rel="stylesheet" href="../css/animate.min.css">
 	<link rel="stylesheet" href="../css/owl.carousel.css">
     <link rel="stylesheet" href="../css/main.css">
-		
+
     </head>
-	
+
     <body id="welcome">
-    
+
         <aside class="left-sidebar">
             <div class="logo">
                 <a href="#welcome">
@@ -69,14 +69,14 @@ session_start();
                 <ul id="nav">
                     <li class="current"><a href="#welcome">Welcome</a></li>
                     <li><a href="#installation">cursos</a></li>
-                  
+
                     <li><a href="cerrarsesion.php">cerrar sesion </a></li>
-                     
-                     
+
+
                 </ul>
             </nav>
         </aside>
-		
+
 		<div id="main-wrapper">
 		    <div class="main-content">
 		        <section id="welcome">
@@ -84,36 +84,36 @@ session_start();
 		                <h1>Bienvenido </h1>
 		            </div>
 		            <div class="welcome">
-                        
+
                         <h2 class="twenty"><?php    echo isset($_SESSION['Nombre'])?$_SESSION['Nombre'] : "no login" ?></h2>
 
                         <p> Tenemos los mejores cursos</p>
 
                         <p> Estudia lo que quieras y cuando quieras </p>
 		            </div>
-		            
+
 		            <div class="features">
 		                <h2 class="twenty">Aqui podras </h2>
-		                
+
 		                <ul>
 		                    <li>ver cursos </li>
-                             
+
 		                </ul>
 		            </div>
 
 		        </section>
-		        
+
 		        <section id="installation">
                     <div class="content-header">
 		                <h1>Cursos</h1>
                     </div>
                     <div class="row">
                     <?php if(count($cursos)>0){
-                        
+
                         foreach ($cursos as $c ) {
-                        
+
                         ?>
-            
+
   <div class="col-sm-3">
                     <div class="card" style="width: 18rem;">
   <img src="img/imgcursos/<?= $c['img']?>" class="card-img-top" alt="...">
@@ -121,21 +121,21 @@ session_start();
     <h5 class="card-title"><?=$c['Nombre']?></h5>
     <p class="card-text"><?=$c['Descripción']?></p>
     <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal<?= $c['id']?>">
-    <i class="fas fa-eye"></i>ver 
+    <i class="fas fa-eye"></i>ver
     </button>
   </div>
- 
+
 
 <!-- Modal -->
 
                     </div>
 
   </div>
-            
+
 
 		            <?php } }else{echo"<h2>No tienes cursos aun </h2>";}?>
                     </div>
-    <?php foreach   ($cursos as $c){?>   
+    <?php foreach   ($cursos as $c){?>
 <div class="modal fade" id="exampleModal<?= $c['id']?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -147,51 +147,51 @@ session_start();
       </div>
       <div class="modal-body">
       <ul class="list-group">
-         <?php $clases=$db->query("select * from clases where id_curso=".$c['id'])->fetch_all(MYSQLI_ASSOC); 
-                    
+         <?php $clases=$db->query("select * from clases where id_curso=".$c['id'])->fetch_all(MYSQLI_ASSOC);
+
                     if (count($clases)>0) {
                         foreach ($clases as $clase  ) {
                             $id=strval( $clase['id']);
-                            echo "<li class='list-group-item'>  {$clase['Nombre']} <a href='"."verclases.php?id=$id' class= 'btn btn-primary'> <i class='fas fa-eye'></i>ver </a> </li>"; 
+                            echo "<li class='list-group-item'>  {$clase['Nombre']} <a href='"."verclases.php?id=$id' class= 'btn btn-primary'> <i class='fas fa-eye'></i>ver </a> </li>";
                          }
                     }else{
-                        echo "<li class='list-group-item'>   no hay clases aun </li>"; 
+                        echo "<li class='list-group-item'>   no hay clases aun </li>";
                     }
                     ?>
-                    
+
 
 </ul>
       </div>
       <div class="modal-footer">
-       
-  
+
+
       </div>
     </div>
   </div>
-</div>  
- 
-    <?php }?>	            
+</div>
+
+    <?php }?>
 		        </section>
 
- 
+
 		<!-- Essential JavaScript Libraries
 		==============================================-->
         <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
         <script type="text/javascript" src="js/jquery.nav.js"></script>
-        <script type="text/javascript" src="syntax-highlighter/scripts/shCore.js"></script> 
-        <script type="text/javascript" src="syntax-highlighter/scripts/shBrushXml.js"></script> 
-        <script type="text/javascript" src="syntax-highlighter/scripts/shBrushCss.js"></script> 
-        <script type="text/javascript" src="syntax-highlighter/scripts/shBrushJScript.js"></script> 
-        <script type="text/javascript" src="syntax-highlighter/scripts/shBrushPhp.js"></script> 
+        <script type="text/javascript" src="syntax-highlighter/scripts/shCore.js"></script>
+        <script type="text/javascript" src="syntax-highlighter/scripts/shBrushXml.js"></script>
+        <script type="text/javascript" src="syntax-highlighter/scripts/shBrushCss.js"></script>
+        <script type="text/javascript" src="syntax-highlighter/scripts/shBrushJScript.js"></script>
+        <script type="text/javascript" src="syntax-highlighter/scripts/shBrushPhp.js"></script>
             <!-- <link rel="shortcut icon" href="img/favicon.png"> -->
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
- 
+
         <script type="text/javascript">
             $('#exampleModal').modal('show')
         </script>
         <script type="text/javascript" src="js/custom.js"></script>
-		
+
     </body>
 </html>
